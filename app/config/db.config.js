142c269b5e488b4b482cf24d5315199734af605d
Jack B <bletsasjack@gmail.com>
2023-08-25 10:59:27 +1000

    commiting the working db configs

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 // These values need to match your SQL database
 
 module.exports = {
-    HOST: "localhost",   // default
-    USER: "root",   // default
-    PASSWORD: "yourSQLPassword",  // your sql password
+    HOST: "%",   // default
+    USER: "asduser",   // default
+    PASSWORD: "Asdproject3d",  // your sql password
     DB: "testdb",  // Has to match the Schema in mysql
     dialect: "mysql",
     pool: {
