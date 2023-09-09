8a8cbd2d4ee4bca0c1cec182b88ae952c96390c5
Mohammed <62224483+Marco2344@users.noreply.github.com>
2023-09-09 17:09:18 +1000

    Revert "comments in main.js"

--- vue-3-frontend/src/main.js
+++ vue-3-frontend/src/main.js
@@ -2,10 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router'
 
-
-createApp(App).mount('#app')
-
-//we will do great things
-
 createApp(App).use(router).mount('#app')
-
