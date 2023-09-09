c7906b74a2f73f9b623bbb608e9b573ea62e75d1
Mohammed <mohammed.misselmany@gmail.com>
2023-09-09 17:58:30 +1000

    Added navbar
    Added navbar, removed the ul styling for the elements in the community page as it was interfering with the navbar bootstrap styling.

--- vue-3-frontend/src/App.vue
+++ vue-3-frontend/src/App.vue
@@ -5,13 +5,34 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-    <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Shop">Shop</router-link> |
-    <router-link to="/CommunityHub">Community Hub</router-link> |
-    <router-link to="/MyAccount">My Account</router-link> |
-    <router-link to="/LogoutCard">Logout</router-link>
-  </div>
+        <div class="p-3 mb-2 bg-white text-dark">
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+<RouterLink class="navbar-brand" to="/Shop">Home</RouterLink>
+
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+  <li class="nav-item">
+    <RouterLink class="nav-link" to="/Shop">Shop</RouterLink>
+  </li>
+  <li class="nav-item">
+    <RouterLink class="nav-link" to="/CommunityHub">Community Hub</RouterLink>
+  </li>
+  <li class="nav-item">
+    <RouterLink class="nav-link" to="/MyAccount">Account</RouterLink>
+  </li>
+  <li class="nav-item">
+    <RouterLink class="nav-link" to="/LogoutCard">Logout</RouterLink>
+  </li>
+</ul>
+</div>
+</div>
+</nav>
+</div>
   <router-view/>
 </template>
 
@@ -31,12 +52,5 @@ export default {
 
 
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
