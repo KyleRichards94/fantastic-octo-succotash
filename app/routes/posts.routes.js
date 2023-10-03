77333e48eebb58060efe66e2febc0105161d576a
Kyle Richards <kyle.richards94@gmail.com>
2023-10-03 17:55:16 +1100

    const posts = require("../controllers/posts.controller.js");     // add cart     //add post     // add comment     // etc. when the model is finished. Simular to the big XML file in javabeans.
        var router = require("express").Router();
    
        //create new post
        router.post("/create", posts.create);
    
        // retrieve all posts
        router.get("/findAll", posts.findAll);
    
        //find one post by id
        router.get("/:postId", posts.findOne);
    
        //find all by title filterd
        router.get("/findFiltered", posts.findFiltered);
    
        //update a single post by id
        router.put("/:postId", posts.update);
    
        //delete a post by Id
        router.delete("/:postId", posts.delete);
    
        // impliment your CRUD
      };

--- app/routes/posts.routes.js
+++ app/routes/posts.routes.js
@@ -6,6 +6,25 @@ module.exports = app => {
     // etc. when the model is finished. Simular to the big XML file in javabeans. 
   
     var router = require("express").Router();
-    
+
+    //create new post
+    router.post("/create", posts.create);
+
+    // retrieve all posts
+    router.get("/findAll", posts.findAll);
+
+    //find one post by id 
+    router.get("/:postId", posts.findOne);
+
+    //find all by title filterd
+    router.get("/findFiltered", posts.findFiltered);
+
+    //update a single post by id
+    router.put("/:postId", posts.update);
+
+    //delete a post by Id
+    router.delete("/:postId", posts.delete);
+
+    app.use('/api/posts', router);
     // impliment your CRUD
   };
