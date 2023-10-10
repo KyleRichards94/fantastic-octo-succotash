462e1c3cd80fef3c20497692725139b15a04db6d
Kyle Richards <kyle.richards94@gmail.com>
2023-10-11 10:42:50 +1100

    changes to store front

--- vue-3-frontend/src/components/ShopFront.vue
+++ vue-3-frontend/src/components/ShopFront.vue
@@ -7,39 +7,50 @@
         <p class="lead" style = "text-align:center;text-shadow:1px 1px white ;font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Find your favourite 3D printers!</p>
     </div>
 
+    <form>
+      <div class="container" style="padding-top: 2%;">
+        <div class="input-group mb-3">
+          <input id='searchBar' type="text" class="form-control" placeholder="Search the Shop"
+            aria-label="Recipient's username" aria-describedby="basic-addon2" v-model="searchQuery">
+          <span class="input-group-text" id="basic-addon2">Search</span>
+        </div>
+      </div>
+      </form>
 
-        <div class="d-flex justify-content-between" style = "padding-top: 2%;">
-            <div class="card" style="width: 25rem;">
-                <img class="card-img-top" style = "background-repeat: no-repeat;background-size: 100% auto;background-position: center center;" src ="https://www.popsci.com/uploads/2023/04/13/best-3d-printer-for-beginners-creality.jpg?auto=webp" alt="Card image cap">
-                <div class="card-body">
-                    <h1>Basic Printer</h1>
-                    <p> Print small things that aren't that special, but fun nonetheless </p>
-                    <hr class="my-4">
-                    <a class="btn btn-success"  href="#" role="button">Purchase</a>
+    <div class="container" style="padding-top: 2%">
+        <div v-for="product in products" :key="product.productId" class="col-md-12">
+            <div class="card" style="margin-bottom: 20px;">
+            <div class="row">
+                <!-- Image Column -->
+                <div class="col-md-4" style = "align-items: center;">
+                <!-- PLACEHOLDER IMAGE -->
+                    <img class="card-img-sm" style="width: 100%" src="https://th.bing.com/th/id/OIP.SsGFYFL4_Xn4BXwx8H3mBwHaE8?pid=ImgDet&rs=1" alt="Card image cap">
+                <!--<img :src="'http://localhost:8090' + product.imageLocation" alt="Card image cap">-->
+                </div>
+                <!-- Text Column -->
+                <div class="col-md-5">
+                    <br>
+                    <h5 class="card-title">{{ product.productName }}</h5>
+                    <hr style="margin: 0; border-top: 3px solid #807b7b;">
+                    <p class="card-text">{{ product.description }}</p>
+                    <hr style="margin: 0; border-top: 3px solid #807b7b;">
+                    <br>
+                </div>
+                <div class = "col-md-3">
+                    <br>
+                    <div class="p-3 border bg-light" style="width: 50%;">
+                        <p class="card-text">$ {{ product.price }}</p>
+                    </div>
+                    <br>
+                    <a href="#" class="btn btn-primary" > Add to cart</a>
+                </div>
+                <div class = "col-md-3">
+                    <span></span>
                 </div>
             </div>
-
-            <div class="card" style="width: 25rem;">
-                <img class="card-img-top" style = "background-repeat: no-repeat;background-size: 100% auto;background-position: center center;" src = 'https://i.pcmag.com/imagery/roundups/075s6pAZ0tD0joFomIaUF6W-2..v1595828696.jpg' alt="Card image cap">
-                <div class="card-body">
-                    <h1>Mid-Tier Printer</h1>
-                    <p> Print with greater accuracy and large component size... now you're cooking with gas!</p>
-                    <hr class="my-4">
-                    <a class="btn btn-success"  href="#" role="button">Purchase</a>
-                </div>
             </div>
-            
-            <div class="card" style="width: 25rem;">
-                <img class="card-img-top" style = "background-repeat: no-repeat;background-size: 100% auto;background-position: center center;" src ='https://i.ytimg.com/vi/ZsA8X-cej9M/maxresdefault.jpg' alt="Card image cap">
-                <div class="card-body">
-                    <h1>High-End/Industrial Printer</h1>
-                    <p> This is the largest printer we sell, it's huge, costs a fortune, but prints life-sized objects with unfathomable precision. </p>
-                    <hr class="my-4">
-                    <a class="btn btn-success"  href="#" role="button">Purchase</a>
-                </div>
-            </div>
-                
-        </div> 
+        </div>
+    </div>
         
     </div>
         <div class="row justify-content-md-center" style = "padding-top: 2%;">
@@ -55,7 +66,35 @@
 
 <script>
 
+    import axios from 'axios';
+    import {ref, onMounted} from 'vue';
+
     export default {
         name: 'ShopFront',
-    }
+        data() {
+
+        },
+        setup() {
+            //Grab all products from the page.
+            const products = ref ([]);
+            axios.get('http://localhost:8090/api/products/findAll').then((response) => {
+                products.value = response.data;
+            });
+
+            onMounted(() => {
+                axios.get('http://localhost:8090/api/products/findAll')
+                    .then((response) => {
+                    products.value = response.data;
+                    })
+                    .catch((error) => {
+                    console.error('Error fetching products:', error);
+                    });
+                });
+
+        return { products };
+        }
+    };
+
+
+    
 </script>
