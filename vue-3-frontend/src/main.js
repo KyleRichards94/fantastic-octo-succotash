8582862799575976a28e7361293af8b805c9b335
Kyle Richards <kyle.richards94@gmail.com>
2023-10-10 18:51:48 +1100

    changes to the way data persistance works

--- vue-3-frontend/src/main.js
+++ vue-3-frontend/src/main.js
@@ -4,4 +4,9 @@ import router from './Router'
 import './assets/main.css'
 import store from '../store'
 
-createApp(App).use(router,store).mount('#app')
+const app = createApp(App);
+
+app.use(router); // Use the router
+app.use(store); // Use the Vuex store
+
+app.mount('#app');
